b570176426eabfa640a3c421c70f475415ab92e8
andresan797 <andresan797@gmail.com>
2026-08-10 14:59:22 +0700

    enw

--- train.py
+++ train.py
@@ -1,17 +1,14 @@
 from ultralytics import YOLO
 
-model = YOLO("yolo11l.pt")
+model = YOLO("yolo11n.pt")
 
 model.train(
     data="data/data.yaml",
     imgsz=640,
     batch=8,
-    epochs=200,        # ← naikkan dari 100
+    epochs=200,       # naikkan epochs untuk compensate model kecil
     workers=1,
     device=0,
-    patience=50,      # ← early stopping
-    augment=True,      # ← tambah augmentasi
-    flipud=0.5,        # ← flip vertical
-    mosaic=1.0,        # ← mosaic augmentation
-    copy_paste=0.1,    # ← copy-paste augmentation
+    patience=50,
+    augment=True,
 )
